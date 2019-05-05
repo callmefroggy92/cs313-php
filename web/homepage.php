d780a12f7758e4b9dd798063515f663d6442ea2d
unknown <loganholland1992@gmail.com>
2019-05-04 21:01:12 -0500

    adding php function

---
+++ web/homepage.php
@@ -0,0 +1,9 @@
+<p> This is the homepage for my page for CS 313 at Brigham Young University - Idaho.  Above you can navigate to the assignments I will be completing this semester as well as a short bio about me, my interests, and my projects. </p>
+
+<p> Counter:
+
+
+	<?php
+		echo "Hello World!";
+	?>
+</p>
