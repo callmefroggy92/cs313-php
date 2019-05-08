bae07de73cea8390bab457cfc58e3b28c2a4b52a
unknown <loganholland1992@gmail.com>
2019-05-08 17:33:09 -0500

    Week 03 Team Activity

---
+++ web/submitted.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+	</head>
+
+	<body>
+		<p> Hello, <?php echo $_POST["name"]; ?><br>
+		Email: <?php echo $_POST["email"]; ?><br>
+		Major: <?php echo $_POST["major"]; ?><br>
+		Comments: <?php echo $_POST["comments"]; ?><br></p>
+	</body>
+</html>
