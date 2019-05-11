c327cd85b057fb24bf77a228b407f95923e682d7
unknown <loganholland1992@gmail.com>
2019-05-11 18:41:56 -0500

    Shopping Cart Assignment

--- web/viewcart.php
+++ web/viewcart.php
@@ -15,16 +15,16 @@
 					<br> Item in cart: <input type='checkbox' name='superdeathrobot' value='Super Death Robot' checked> </p> ";
 			} if(isset($_POST["evilchair"])){
 				echo "<p> Super Death Robot -- $5,000 
-					<br> Item in cart: <input type='checkbox' name='superdeathrobot' value='Super Death Robot' checked> </p> ";
+					<br> Item in cart: <input type='checkbox' name='evilchair' value='Evil High-backed Chair' checked> </p> ";
 			} if(isset($_POST["spikepit"])){
 				echo "<p> Super Death Robot -- $75,000 
-					<br> Item in cart: <input type='checkbox' name='superdeathrobot' value='Super Death Robot' checked> </p> ";
+					<br> Item in cart: <input type='checkbox' name='spikepit' value='Spike Pit' checked> </p> ";
 			} if(isset($_POST["jellyfishtank"])){
 				echo "<p> Super Death Robot -- $50,000 
-					<br> Item in cart: <input type='checkbox' name='superdeathrobot' value='Super Death Robot' checked> </p> ";
+					<br> Item in cart: <input type='checkbox' name='jellyfishtank' value='Jellyfish Tank' checked> </p> ";
 			} if(isset($_POST["selfdestructbutton"])){
 				echo "<p> Super Death Robot -- $1,000 
-					<br> Item in cart: <input type='checkbox' name='superdeathrobot' value='Super Death Robot' checked> </p> ";
+					<br> Item in cart: <input type='checkbox' name='selfdestructbutton' value='Self Destruct Button' checked> </p> ";
 			}
 			echo "<input type='submit' value='Update Cart' formaction='viewcart.php'> <input type='submit' value='Proceed to Checkout' formaction='checkout.php'></form>"
 		?>
