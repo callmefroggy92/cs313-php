2afbf811158b2412575a10fb1bd4faa9305f507c
unknown <loganholland1992@gmail.com>
2019-05-11 18:20:14 -0500

    Shopping Cart Assignment

--- web/viewcart.php
+++ web/viewcart.php
@@ -1,26 +1,22 @@
 <html>
 	<head>
+	
 	</head>
 
 	<body>
-		<form action="checkout.php" method="post">
-			<?php
-				echo "Shopping cart: <br> <form action='viewcart.php' method='post'>";
-				echo $_POST["test"];
-				if(isset($_POST["superdeathray"])){
-					echo "<p> Super Death Ray -- $1,000,000 
-						<br> Item in cart: <input type='checkbox' name='superdeathray' checked> </p>";
+		<?php
+			echo "Shopping cart: <br> <form action='viewcart.php' method='post'>";
+			echo $_POST["test"];
+			if(isset($_POST["superdeathray"])){
+				echo "<p> Super Death Ray -- $1,000,000 
+					<br> Item in cart: <input type='checkbox' name='superdeathray' value='Super Death Ray' checked> </p>";
 				}
 			
-				if(isset($_POST["superdeathrobot"])){
-					echo "<p> Super Death Robot -- $250,000 
-						<br> Item in cart: <input type='checkbox' name='superdeathrobot' checked> </p> ";
-				}
-				echo "<input type='submit' value='Update Cart'></form>"
-			?>
-
-			<input type='submit' value='Proceed to Checkout'>
-		</form>
-
+			if(isset($_POST["superdeathrobot"])){
+				echo "<p> Super Death Robot -- $250,000 
+					<br> Item in cart: <input type='checkbox' name='superdeathrobot' value='Super Death Robot' checked> </p> ";
+			}
+			echo "<input type='submit' value='Update Cart' form.action='viewcart.php'> <input type='submit' value='Proceed to Checkout' form.action='checkoutphp'></form>"
+		?>
 	</body>
 </html>
