c42414e4512e5aaa988a87453f59b0530ce2a334
unknown <loganholland1992@gmail.com>
2019-05-11 16:42:34 -0500

    Shopping Cart Assignment

--- web/viewcart.php
+++ web/viewcart.php
@@ -9,14 +9,14 @@
 			echo $_POST["test"];
 			if(isset($_POST["superdeathray"])){
 				echo "<p> Super Death Ray -- $1,000,000 
-					<br> <input type='checkbox' name='superdeathray' ischeck=true> </p>";
+					<br> Item in cart: <input type='checkbox' name='superdeathray' checked> </p>";
 			}
 			
 			if(isset($_POST["superdeathrobot"])){
 				echo "<p> Super Death Robot -- $250,000 
-					<br> <input type='checkbox' name='superdeathrobot' ischecked=true> </p> ";
+					<br> Item in cart: <input type='checkbox' name='superdeathrobot' checked> </p> ";
 			}
-			echo "<input type='submit'></form>"
+			echo "<input type='submit' value='Update Cart'></form>"
 		?>
 
 	</body>
