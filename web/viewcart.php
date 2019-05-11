466f2aa465c5094afa8fd2416d634fbf9bef79cb
unknown <loganholland1992@gmail.com>
2019-05-11 16:14:11 -0500

    Shopping Cart Assignment

--- web/viewcart.php
+++ web/viewcart.php
@@ -6,10 +6,12 @@
 		
 		<?php
 			echo "Shopping cart: <br>";
-			if(isset($_POST["superdeathray"]))
+			if(isset($_POST["superdeathray"])){
 				echo "Super Death Ray -- $1,000,000";
-			if(isset($_POST["superdeathrobot"]))
+			}
+			if(isset($_POST["superdeathrobot"])){
 				echo "Super Death Robot -- $250,000";
+			}
 		?>
 
 	</body>
