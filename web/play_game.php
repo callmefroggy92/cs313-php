9746327fc75b00fadb6ddfafbf481795e0cbba00
unknown <loganholland1992@gmail.com>
2019-05-24 18:37:44 -0500

    adding access to database

--- web/play_game.php
+++ web/play_game.php
@@ -5,28 +5,22 @@
 	<BODY>
 
 		<?php
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
+			try
+			{
+  				$dbUrl = getenv('DATABASE_URL');
 
-  $dbOpts = parse_url($dbUrl);
+  				$dbOpts = parse_url($dbUrl);
 
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
+  				$dbHost = $dbOpts["host"];
+  				$dbPort = $dbOpts["port"];
+  				$dbUser = $dbOpts["user"];
+  				$dbPassword = $dbOpts["pass"];
+  				$dbName = ltrim($dbOpts["path"],'/');
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  				$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-	echo 'Success!';
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+				echo 'Success!';
+  				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 			foreach ($db->query('SELECT * FROM player') as $row)
 			{
@@ -34,6 +28,13 @@ catch (PDOException $ex)
 				echo 'Player Name: ' . $row['name'];
 				echo ' Faction: ' . $row['faction'];
 				echo '<br/>';
+			}
+
+			}
+			catch (PDOException $ex)
+			{
+  				echo 'Error!: ' . $ex->getMessage();
+  				die();
 			}
 		?>
 	</BODY>
