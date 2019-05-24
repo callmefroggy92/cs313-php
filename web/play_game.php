1f305b52c7134aae3ef08fb5a0222a7438044530
unknown <loganholland1992@gmail.com>
2019-05-24 18:56:25 -0500

    adding access to database

--- web/play_game.php
+++ web/play_game.php
@@ -19,12 +19,10 @@
 
   				$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-				echo 'Success!';
   				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 			foreach ($db->query('SELECT * FROM player') as $row)
 			{
-				echo 'test';
 				echo 'Player Name: ' . $row['name'];
 				echo ' Faction: ' . $row['faction'];
 				echo '<br/>';
