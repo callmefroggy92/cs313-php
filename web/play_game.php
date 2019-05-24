d3bb53f5b52940eaa595f12dfcd469090ba61f0b
unknown <loganholland1992@gmail.com>
2019-05-24 18:24:15 -0500

    adding access to database

--- web/play_game.php
+++ web/play_game.php
@@ -5,28 +5,22 @@
 	<BODY>
 
 		<?php
-			try
-			{
- 				$dbUrl = getenv('DATABASE_URL');
+try
+{
+  $user = 'postgres';
+  $password = 'password';
+  $db = new PDO('pgsql:host=localhost;dbname=Logan', $user, $password);
 
-				$dbOpts = parse_url($dbUrl);
-
-				$dbHost = $dbOpts["host"];
-				$dbPort = $dbOpts["port"];
-				$dbUser = $dbOpts["user"];
-				$dbPassword = $dbOpts["pass"];
-				$dbName = ltrim($dbOpts["path"],'/');
-
-				$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			}
-			catch (PDOException $ex)
-			{
-  				echo 'Error!: ' . $ex->getMessage();
-  				die();
-			}
-
+  // this line makes PDO give us an exception when there are problems,
+  // and can be very helpful in debugging! (But you would likely want
+  // to disable it for production environments.)
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
 			foreach ($db->query('SELECT * FROM player') as $row)
 			{
 				echo 'Player Name: ' . $row['ame'];
