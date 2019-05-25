92ab340c1abc52a918e0c58a87c81dbeb4dd3a22
unknown <loganholland1992@gmail.com>
2019-05-25 06:00:57 -0500

    adding access to database

--- web/play_game.php
+++ web/play_game.php
@@ -45,7 +45,7 @@
 				echo 'Player Name: ' . $row['name'];
 				echo ' Faction: ' . $row['faction'];
 				$army = explode(" ", $row['army']);
-				echo $army;
+				echo $row['army'];
 				foreach($army as $line){
 					$c = new Character($row['army']);
 					$c->show();
