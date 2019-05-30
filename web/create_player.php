7808bea189452e76e8b46aab4906282b51f06581
unknown <loganholland1992@gmail.com>
2019-05-30 16:54:26 -0500

    Week 06

---
+++ web/create_player.php
@@ -0,0 +1,46 @@
+<HTML>
+	<HEAD>
+
+	</HEAD>
+
+	<BODY>
+		<form action="createPlayer()" method="post">
+			<input type="text" name="name">
+			<input type="text" name="faction">
+			<input type="text" name="army">
+			<input type="submit">
+		</form>
+		<?php
+			function createPlayer(){
+			try{
+  				$dbUrl = getenv('DATABASE_URL');
+
+  				$dbOpts = parse_url($dbUrl);
+
+  				$dbHost = $dbOpts["host"];
+  				$dbPort = $dbOpts["port"];
+  				$dbUser = $dbOpts["user"];
+  				$dbPassword = $dbOpts["pass"];
+  				$dbName = ltrim($dbOpts["path"],'/');
+
+  				$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				$name = $_POST["name"];
+				$faction = $_POST["faction"];
+				$army = $_POST["army"];
+				$id = rand(00000,99999);
+				$vp = 0;
+				$db->exec("INSERT INTO player VALUES('$name','$faction','$army','$id','vp');");
+
+				
+			}
+			catch (PDOException $ex){
+  				echo 'Error!: ' . $ex->getMessage();
+  				die();
+			}
+			}
+		?>
+
+	</BODY>
+</HTML>
