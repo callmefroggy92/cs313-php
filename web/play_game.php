fdfe6fe254f0f4fc047b902748bbc620313d4c7e
unknown <loganholland1992@gmail.com>
2019-06-07 15:11:45 -0500

    Week 06

--- web/play_game.php
+++ web/play_game.php
@@ -16,83 +16,67 @@
 		</style>
 
 		<script>
+			function unit(race, class, health, des){
+				this.race = race;
+				this.class = class;
+				this.health = health;
+				this.des = des;
+			}
 
+			function create_army(str){
+				var army = [];
+				var army_str = str.split(" ");
+				var i;
+				for(i = 0; i < army.length; i++){
+				var race;
+				var class;
+				var health;
+				var des;
+					if(army_str[i][0] == "h"){
+						race = "Human";
+						if(army_str[i][1] == "w")
+							class = "Warrior";
+						else if(army_str[i][1] == "s")
+							class = "Soldier";
+						else if(army_str[i][1] == "r")
+							class = "Sniper";
+					}
+					else if(army_str[i][0] == "x"){
+						race = "Xenos";	
+							if(army_str[i][1] == "w")
+								class = "Warrior";
+							else if(army_str[i][1] == "s"){
+								class = "Soldier";
+							}	
+							else if(army_str[i][1] == "r"){
+								class = "Sniper";
+							}
+					}
+				var u = new Unit(race, class, 0, "test");
+				army.push(u);
+				}
+			return army;			
+			}			
+				
+			function create_player(){
+				var player_content = 
+					"<h3> 
+						<?php echo get_player1_name(); ?> 
+					</h3>
+					<br><br>
+					<h4> 
+						<?p echo get_player1_faction(); ?>
+					</h4>";
+
+				document.getElementById("player1").innerHTML = player_content;
+			} 
+											
 		</script>
 	</HEAD>
 
-	<BODY>
+	<BODY onload="create_player()">
 
 		<?php
-
-			class Character{
-
-				public function __construct($str){
-					$this->health = intval($str[2]);
-					if ($str[0] == 'h'){
-						$this->race = 'Human';
-					
-						if ($str[1] == 's'){
-							$this->class = 'Solider';
-							$this->desc = "A powerful and trained human soldier, disciplined in the arts of shooting and guerrila warfare.";
-						}
-						elseif($str[1] == 'w'){
-							$this->class = 'Warrior';
-							$this->desc = "A power warrior, specializing in all forms of hand-to-hand combat.";
-						}
-						elseif($str[1] == 'r'){
-							$this->class = "Sniper";
-							$this->desc = "A human trained in covert ops.  A sniper specializes in taking out high-priority targets.";
-						}
-					}
-
-					elseif($str[0] == 'x'){
-						$this->race = 'Xenos';
-				
-						if ($str[1] == 's'){
-							$this->class = 'Soldier';
-							$this->desc = "A foot soldier for the Xenos, wielding advanced weaponry.";
-						}
-						elseif ($str[1] == 'w'){
-							$this->class = 'Warrior';
-							$this->desc = "A Xenos brute.  He has strength and speed surpassing any creature native to Terra.";
-						}
-					}
-				}
-
-				public function show(){
-					echo "<br><br>" . $this->race . " -- " . $this->class;
-					echo "<br> Health Remaining: " . $this->health;
-					echo "<br> " . $this->desc;
-					
-				}
-
-				public function return_unit(){
-					$unit = "  kjlj  ";
-
-
-					echo $unit;
-
-					return $unit;
-				}	
-
-			}
-
-			$army = array();
-		
-			function return_army(){
-				$str = "";
-				foreach($army as $unit){
-					$str += $unit->return_unit();
-				}
-
-				echo "test";
-				echo $army;
-				return $str;
-			}
-
-
-			try
-			{
   				$dbUrl = getenv('DATABASE_URL');
 
   				$dbOpts = parse_url($dbUrl);
@@ -107,34 +91,45 @@
 
   				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-			$player1 = (int)$_GET["player1"];
-			$player2 = (int)$_GET["player2"];
-			foreach ($db->query("SELECT * FROM player WHERE id='$player1' or id='$player2'") as $row){
-				echo "<div id='player'>";
-				echo '<h1>';
-				echo 'Player Name: ' . $row['name'];
-				echo '</h1><h2>';
-				echo ' Faction: ' . $row['faction'];
-				echo '</h2>';
-				$army = explode(" ", $row['army']);				
+			function get_player1_name(){
+				$player1 = (int)$_GET["player1"];
+				echo $db->query("SELECT name FROM player WHERE id='$player1'");
+			}
 
-				foreach($army as $line){
-					$c = new Character($line);
-					array_push($army,$c->return_unit());
-					$c->show();
-					echo '<br>' . return_army() . '<br>';
-				}
-				echo "<br><br></div>";
-	
-				echo return_army();
+			function get_player1_faction(){
+				$player1 = (int)$_GET["player1"];
+				echo $db->query("SELECT faction FROM player WHERE id='$player1'");
+			}
+			
+			function get_player1_army(){
+				$player1 = (int)$_GET["player1"];
+				echo $db->query("SELECT army FROM player WHERE id='$player1'");
 			}			
 
+			function get_player2_name(){
+				$player2 = (int)$_GET["player2"];
+				echo $db->query("SELECT name FROM player WHERE id='$player2'");
 			}
 
-			catch (PDOException $ex){
-  				echo 'Error!: ' . $ex->getMessage();
-  				die();
+			function get_player2_faction(){
+				$player2 = (int)$_GET["player2"];
+				echo $db->query("SELECT faction FROM player WHERE id='$player2'");
 			}
+			
+			function get_player2_army(){
+				$player2 = (int)$_GET["player2"];
+				echo $db->query("SELECT army FROM player WHERE id='$player2'");
+			}			
+
 		?>
+
+		<div id="player1" class="player">
+		</div>
+
+		<br><br>
+
+		<div id="player2" class="player">
+		</div>
+
 	</BODY>
 </HTML>	
