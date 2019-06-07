ad0c9c2db3d02233a166f80d804b68c0e9eb6c35
unknown <loganholland1992@gmail.com>
2019-06-07 15:24:58 -0500

    Week 06

--- web/play_game.php
+++ web/play_game.php
@@ -80,53 +80,6 @@
 
 	<BODY>
 
-		<?php
-  			$dbUrl = getenv('DATABASE_URL');
-
-  			$dbOpts = parse_url($dbUrl);
-
-  			$dbHost = $dbOpts["host"];
-  			$dbPort = $dbOpts["port"];
-  			$dbUser = $dbOpts["user"];
-  			$dbPassword = $dbOpts["pass"];
-  			$dbName = ltrim($dbOpts["path"],'/');
-
-  			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-			function get_player1_name(){
-				$player1 = (int)$_GET["player1"];
-				echo $db->query("SELECT name FROM player WHERE id='$player1'");
-			}
-
-			function get_player1_faction(){
-				$player1 = (int)$_GET["player1"];
-				echo $db->query("SELECT faction FROM player WHERE id='$player1'");
-			}
-			
-			function get_player1_army(){
-				$player1 = (int)$_GET["player1"];
-				echo $db->query("SELECT army FROM player WHERE id='$player1'");
-			}			
-
-			function get_player2_name(){
-				$player2 = (int)$_GET["player2"];
-				echo $db->query("SELECT name FROM player WHERE id='$player2'");
-			}
-
-			function get_player2_faction(){
-				$player2 = (int)$_GET["player2"];
-				echo $db->query("SELECT faction FROM player WHERE id='$player2'");
-			}
-			
-			function get_player2_army(){
-				$player2 = (int)$_GET["player2"];
-				echo $db->query("SELECT army FROM player WHERE id='$player2'");
-			}			
-
-		?>
-
 		<div id="player1" class="player">
 		</div>
 
