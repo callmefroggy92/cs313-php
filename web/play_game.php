7c03ddeb16dd59e29c1b1802c5c7173e8341ac99
unknown <loganholland1992@gmail.com>
2019-06-07 15:23:47 -0500

    Week 06

--- web/play_game.php
+++ web/play_game.php
@@ -81,19 +81,19 @@
 	<BODY>
 
 		<?php
-  				$dbUrl = getenv('DATABASE_URL');
+  			$dbUrl = getenv('DATABASE_URL');
 
-  				$dbOpts = parse_url($dbUrl);
+  			$dbOpts = parse_url($dbUrl);
 
-  				$dbHost = $dbOpts["host"];
-  				$dbPort = $dbOpts["port"];
-  				$dbUser = $dbOpts["user"];
-  				$dbPassword = $dbOpts["pass"];
-  				$dbName = ltrim($dbOpts["path"],'/');
+  			$dbHost = $dbOpts["host"];
+  			$dbPort = $dbOpts["port"];
+  			$dbUser = $dbOpts["user"];
+  			$dbPassword = $dbOpts["pass"];
+  			$dbName = ltrim($dbOpts["path"],'/');
 
-  				$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  			$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-  				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 			function get_player1_name(){
 				$player1 = (int)$_GET["player1"];
