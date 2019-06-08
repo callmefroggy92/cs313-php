aa68e37f6b5cca5622f98e02511950eb9e33906d
unknown <loganholland1992@gmail.com>
2019-06-08 08:26:46 -0500

    Week 06

--- web/play_game.php
+++ web/play_game.php
@@ -140,6 +140,8 @@
 						$c->show();
 						echo '<br/>';
 					}
+				$points1 = $db->query("SELECT victory_points FROM player WHERE id='$player1'");
+				$points2 = $db->query("SELECT victory_points FROM player WHERE id='$player2'");
 
 				echo "<br><br></div>";
 				}
