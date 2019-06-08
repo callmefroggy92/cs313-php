da932d4b9c0e036c472c74aad7e5eff5e66e5ebf
unknown <loganholland1992@gmail.com>
2019-06-08 07:56:42 -0500

    Week 06

--- web/play_game.php
+++ web/play_game.php
@@ -22,16 +22,31 @@
   				die();
 			} 
 			?>
-		
-			function loadP1Points(){
-				p1Points = <?php
-					$id = (int)$_GET["player1"];
-					echo $db->query("SELECT victory_points FROM player WHERE id='$id'");
-					?>
+			function updateP1Points(inc){
 				p1 = document.getElementById("p1");
-				p1.innerHTML = p1Points;
+				p1Points = 0;
+				if(inc == '+')
+					p1Points = <?php
+						$id = (int)$_GET["player1"];
+						$points = ((int)$db->query("SELECT victory_points FROM player WHERE id='$id'")) + 1;
+						db->query("UPDATE player SET victory_points='$points' WHERE id='$id');
+						?>
+					loadP1Points();
 				}
-		
+
+				else if(inc == '-'){
+				p1 = document.getElementById("p1");
+				p1Points = 0;
+				if(inc == '+')
+					p1Points = <?php
+						$id = (int)$_GET["player1"];
+						$points = ((int)$db->query("SELECT victory_points FROM player WHERE id='$id'")) - 1;
+						db->query("UPDATE player SET victory_points='$points' WHERE id='$id');
+						?>
+					loadP1Points();
+				}
+
+			}		
 					
 			
 		</script>
