d280cede16668e84477d895d30dbb98c20fb3ce2
unknown <loganholland1992@gmail.com>
2019-06-22 10:30:57 -0500

    First Commit

--- web/app.js
+++ web/app.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 5000
 
 app.get('/postage',function(req,res) {
       res.sendFile('postage.html', {
